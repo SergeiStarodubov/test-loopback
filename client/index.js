028ff9143680b9c46b47445a74f4c190928c54c5
Starodubov_S <sergey@anmedio.ru>
2019-06-05 18:51:10 +0400

    fixing routes

--- client/index.js
+++ client/index.js
@@ -1,21 +1,23 @@
 document.querySelector("button").addEventListener("click", () => {
-  fetch("/api/Developers", {
+  const email = document.querySelector("#email");
+  const password = document.querySelector("#password")
+  fetch("/api/Writers/login", {
     method: "post",
     headers: new Headers({
       "Accept": "application/json",
       "Content-Type": "application/json"
     }),
     body: JSON.stringify({
-      "skills": [
-        "css"
-      ],
-      "age": 5,
-      "username": "john",
-      "email": "twen@yandex.ru",
-      "emailVerified": false,
-      "password": "helloworld"
+      "email": email.value,
+      "password" : password.value
     })
-  }).catch((err) => {
-    console.error(err);
-  })
+  }).then((response) => response.json())
+    .then((response) => {
+      const token = response.id;
+      if (token === undefined) return;
+      localStorage.setItem("swagger_accessToken", token);
+      document.location.href = "http://localhost:3000/index.html";
+    }).catch((err) => {
+      console.error(err);
+    })
 });
--- server/boot/script.js
+++ server/boot/script.js
@@ -9,27 +9,17 @@ module.exports = function(app) {
   User.create([
     {username: 'Mark', email: 'markTwen@gmail.com', password: '12345', age: '30', skills: ['css']},
   ], function(err, users) {
-    // eslint-disable-next-line no-undef
     if (err) console.error(err);
-    // ...
-    // Create projects, assign project owners and project team members
-    // ...
-    // Create the admin role
     Role.create({
       name: 'admin',
     }, function(err, role) {
-      // eslint-disable-next-line no-undef
       if (err) console.error(err);
-      // eslint-disable-next-line no-undef
       console.info(role);
-      // Make Bob an admin
       role.principals.create({
         principalType: RoleMapping.USER,
         principalId: users[0].id,
       }, function(err, principal) {
-        // eslint-disable-next-line no-undef
         if (err) console.error(err);
-        // eslint-disable-next-line no-undef
         console.info(principal);
       });
     });
